3b42fc702721c3674c525b7dbf458ef6553127d4
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2025-10-10 20:56:30 +0000

    Primeira aula 1:28:23

---
+++ web/src/pages/create-room.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query"
+import { Link } from "react-router-dom"
+
+type GetRoomsAPIResponse = Array<{ id: string, name: string }>
+
+
+export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms")
+      const result: GetRoomsAPIResponse = await response.json()
+
+      return result
+    }
+  })
+
+  return (
+    <div>
+      <div>
+        Create Room
+      </div>
+
+      {
+        isLoading && <p>Carregando...</p>
+      }
+
+      <Link to={"/room"}>Acessar sala</Link>
+
+    </div>
+  )
+}
