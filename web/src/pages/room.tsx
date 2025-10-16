24d0429595bf17b3d0f082244beaada2c3c32bba
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2025-10-16 22:40:31 +0000

    Primeira aula finalizada, prosseguindo para segunda

--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,5 +1,17 @@
+import { Navigate, useParams } from "react-router-dom"
+
+type paramsProps = {
+  id: string  
+}
+
 export function Room(){
+  const params = useParams<paramsProps>()
+
+  if(!params.id){
+    return <Navigate replace to="/"/>
+  }
+  
   return (
-    <div>Room details</div>
+    <div>Room details{JSON.stringify(params)}</div>
   )
 }
